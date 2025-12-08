5735e6dad453bf901dc5fd5acd3edcc0201191c8
AlexeyZam15 <alexeyzam15@mail.ru>
2025-12-08 21:52:23 +1000

    feat: add comprehensive platform icon system with 60+ platforms
    - Add support for all major gaming platforms from retro to modern
    - Generate SVG icons with color coding by platform family
    - Include multiple aliases for each platform name
    - Download Android icon, generate others locally

--- igdb_site/games/management/commands/create_platform_icons.py
+++ igdb_site/games/management/commands/create_platform_icons.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
         # Сначала скачиваем Android иконку
         self.download_android_icon(icons_dir)
 
-        # Затем создаем остальные иконки
+        # Создаем остальные иконки
         icons = {
             # PlayStation
             'playstation.svg': self.create_platform_icon('PS1', '#003087'),
@@ -24,6 +24,8 @@ class Command(BaseCommand):
             'playstation5.svg': self.create_platform_icon('PS5', '#003087'),
             'psp.svg': self.create_platform_icon('PSP', '#003087'),
             'vita.svg': self.create_platform_icon('VITA', '#003087'),
+            'psvr.svg': self.create_platform_icon('PS VR', '#003087'),
+            'psvr2.svg': self.create_platform_icon('PSVR2', '#003087'),
 
             # Xbox
             'xbox.svg': self.create_platform_icon('XBOX', '#107c10'),
@@ -37,15 +39,85 @@ class Command(BaseCommand):
             'wii-u.svg': self.create_platform_icon('WII U', '#e60012'),
             'nintendo-3ds.svg': self.create_platform_icon('3DS', '#e60012'),
             'nintendo-ds.svg': self.create_platform_icon('DS', '#e60012'),
-            'game-boy.svg': self.create_platform_icon('GAME BOY', '#e60012'),
+            'game-boy.svg': self.create_platform_icon('GB', '#e60012'),
+            'gameboy-color.svg': self.create_platform_icon('GBC', '#e60012'),
+            'gba.svg': self.create_platform_icon('GBA', '#e60012'),
+            'nintendo64.svg': self.create_platform_icon('N64', '#e60012'),
+            'gamecube.svg': self.create_platform_icon('GCN', '#e60012'),
+            'nes.svg': self.create_platform_icon('NES', '#e60012'),
+            'snes.svg': self.create_platform_icon('SNES', '#e60012'),
+            'satellaview.svg': self.create_platform_icon('BS-X', '#e60012'),
+            'fds.svg': self.create_platform_icon('FDS', '#e60012'),
+            '64dd.svg': self.create_platform_icon('64DD', '#e60012'),
+
+            # Sega
+            'dreamcast.svg': self.create_platform_icon('DC', '#1e6ca8'),
+            'saturn.svg': self.create_platform_icon('SAT', '#1e6ca8'),
+            'megadrive.svg': self.create_platform_icon('MD', '#1e6ca8'),
+            'segacd.svg': self.create_platform_icon('SCD', '#1e6ca8'),
+            'gamegear.svg': self.create_platform_icon('GG', '#1e6ca8'),
 
             # PC
             'windows.svg': self.create_platform_icon('PC', '#0078d7'),
+            'dos.svg': self.create_platform_icon('DOS', '#0078d7'),
             'linux.svg': self.create_platform_icon('LINUX', '#ff6600'),
             'macos.svg': self.create_platform_icon('MAC', '#000000'),
 
             # Mobile
             'ios.svg': self.create_platform_icon('iOS', '#000000'),
+            'windowsphone.svg': self.create_platform_icon('WP', '#0078d7'),
+            'blackberry.svg': self.create_platform_icon('BB', '#000000'),
+            'mobile.svg': self.create_platform_icon('MOB', '#666666'),
+
+            # Retro/Classic
+            'atari2600.svg': self.create_platform_icon('2600', '#8b4513'),
+            'jaguar.svg': self.create_platform_icon('JAG', '#000000'),
+            'atarist.svg': self.create_platform_icon('ST', '#8b4513'),
+            'atari8bit.svg': self.create_platform_icon('A8', '#8b4513'),
+            'c64.svg': self.create_platform_icon('C64', '#4169e1'),
+            'amiga.svg': self.create_platform_icon('AMIGA', '#000000'),
+            'zxspectrum.svg': self.create_platform_icon('ZX', '#ff0000'),
+            'amstradcpc.svg': self.create_platform_icon('CPC', '#800080'),
+            'msx.svg': self.create_platform_icon('MSX', '#4682b4'),
+            'intellivision.svg': self.create_platform_icon('INTV', '#8b0000'),
+            'ngpc.svg': self.create_platform_icon('NGPC', '#ff4500'),
+            'pcengine.svg': self.create_platform_icon('PCE', '#ff69b4'),
+            'pcenginecd.svg': self.create_platform_icon('PCE-CD', '#ff69b4'),
+
+            # Arcade
+            'arcade.svg': self.create_platform_icon('ARC', '#dc143c'),
+
+            # Web/Cloud
+            'webbrowser.svg': self.create_platform_icon('WEB', '#4285f4'),
+            'stadia.svg': self.create_platform_icon('STADIA', '#008744'),
+            'onlive.svg': self.create_platform_icon('ONLIVE', '#00a8ff'),
+
+            # VR
+            'oculus.svg': self.create_platform_icon('OCULUS', '#1c1e20'),
+            'gearvr.svg': self.create_platform_icon('GEAR VR', '#34a853'),
+            'daydream.svg': self.create_platform_icon('DAYDREAM', '#4285f4'),
+            'steamvr.svg': self.create_platform_icon('VR', '#1b2838'),
+
+            # Other
+            '3do.svg': self.create_platform_icon('3DO', '#ff0000'),
+            'acorn.svg': self.create_platform_icon('ACORN', '#800000'),
+            'appleii.svg': self.create_platform_icon('APPLE II', '#66bb6a'),
+            'bbc.svg': self.create_platform_icon('BBC', '#000080'),
+            'cdtv.svg': self.create_platform_icon('CDTV', '#4169e1'),
+            'pet.svg': self.create_platform_icon('PET', '#4169e1'),
+            'vic20.svg': self.create_platform_icon('VIC-20', '#4169e1'),
+            'fmtowns.svg': self.create_platform_icon('FM TOWNS', '#4682b4'),
+            'fm7.svg': self.create_platform_icon('FM-7', '#4682b4'),
+            'amico.svg': self.create_platform_icon('AMICO', '#ff6b6b'),
+            'ngage.svg': self.create_platform_icon('N-GAGE', '#7b68ee'),
+            'ouya.svg': self.create_platform_icon('OUYA', '#66bb6a'),
+            'pc88.svg': self.create_platform_icon('PC-88', '#4682b4'),
+            'pc98.svg': self.create_platform_icon('PC-98', '#4682b4'),
+            'sharp.svg': self.create_platform_icon('SHARP', '#808080'),
+            'x68000.svg': self.create_platform_icon('X68000', '#808080'),
+            'trs80.svg': self.create_platform_icon('TRS-80', '#8b4513'),
+            'wonderswan.svg': self.create_platform_icon('WS', '#ff69b4'),
+            'wonderswancolor.svg': self.create_platform_icon('WSC', '#ff69b4'),
 
             # Default
             'default.svg': self.create_platform_icon('🎮', '#6c757d'),
--- igdb_site/games/templatetags/platform_icons.py
+++ igdb_site/games/templatetags/platform_icons.py
@@ -3,11 +3,11 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+
 @register.simple_tag
 def platform_icon(platform_name):
     """Возвращает иконку платформы"""
     name_lower = platform_name.lower()
-
     static_url = "/static/games/platform_icons/"
 
     platform_map = {
@@ -26,10 +26,13 @@ def platform_icon(platform_name):
         'playstation portable': 'psp.svg',
         'vita': 'vita.svg',
         'playstation vita': 'vita.svg',
+        'playstation vr': 'psvr.svg',
+        'playstation vr2': 'psvr2.svg',
 
         # Xbox
         'xbox series x': 'xbox-series-x.svg',
         'xbox series s': 'xbox-series-x.svg',
+        'xbox series x|s': 'xbox-series-x.svg',
         'xbox one': 'xbox-one.svg',
         'xbox 360': 'xbox-360.svg',
         'xbox': 'xbox.svg',
@@ -37,18 +40,46 @@ def platform_icon(platform_name):
         # Nintendo
         'nintendo switch': 'nintendo-switch.svg',
         'switch': 'nintendo-switch.svg',
+        'nintendo switch 2': 'nintendo-switch.svg',
         'wii u': 'wii-u.svg',
         'wii': 'wii.svg',
         'nintendo 3ds': 'nintendo-3ds.svg',
         '3ds': 'nintendo-3ds.svg',
+        'new nintendo 3ds': 'nintendo-3ds.svg',
         'nintendo ds': 'nintendo-ds.svg',
         'ds': 'nintendo-ds.svg',
+        'nintendo dsi': 'nintendo-ds.svg',
+        'nintendo 64': 'nintendo64.svg',
+        'nintendo gamecube': 'gamecube.svg',
+        'gamecube': 'gamecube.svg',
+        'nintendo entertainment system': 'nes.svg',
+        'nes': 'nes.svg',
+        'family computer': 'nes.svg',
+        'super nintendo entertainment system': 'snes.svg',
+        'snes': 'snes.svg',
+        'super famicom': 'snes.svg',
         'game boy': 'game-boy.svg',
+        'game boy color': 'gameboy-color.svg',
+        'game boy advance': 'gba.svg',
+        'gba': 'gba.svg',
+        'satellaview': 'satellaview.svg',
+        'family computer disk system': 'fds.svg',
+        '64dd': '64dd.svg',
+
+        # Sega
+        'dreamcast': 'dreamcast.svg',
+        'sega saturn': 'saturn.svg',
+        'sega mega drive': 'megadrive.svg',
+        'sega genesis': 'megadrive.svg',
+        'sega cd': 'segacd.svg',
+        'sega game gear': 'gamegear.svg',
 
         # PC
         'windows': 'windows.svg',
         'pc': 'windows.svg',
         'microsoft windows': 'windows.svg',
+        'pc (microsoft windows)': 'windows.svg',
+        'dos': 'dos.svg',
         'linux': 'linux.svg',
         'mac': 'macos.svg',
         'macos': 'macos.svg',
@@ -56,12 +87,98 @@ def platform_icon(platform_name):
         'macintosh': 'macos.svg',
 
         # Mobile
-        'android': 'android.png',  # Теперь PNG
+        'android': 'android.png',
         'ios': 'ios.svg',
         'iphone': 'ios.svg',
         'ipad': 'ios.svg',
+        'windows phone': 'windowsphone.svg',
+        'blackberry os': 'blackberry.svg',
+        'blackberry': 'blackberry.svg',
+        'legacy mobile device': 'mobile.svg',
+
+        # Retro/Classic
+        'atari 2600': 'atari2600.svg',
+        'atari jaguar': 'jaguar.svg',
+        'atari st': 'atarist.svg',
+        'atari ste': 'atarist.svg',
+        'atari 8-bit': 'atari8bit.svg',
+        'commodore 64': 'c64.svg',
+        'commodore 128': 'c64.svg',
+        'commodore max': 'c64.svg',
+        'commodore c64/128/max': 'c64.svg',
+        'amiga': 'amiga.svg',
+        'amiga cd32': 'amiga.svg',
+        'zx spectrum': 'zxspectrum.svg',
+        'amstrad cpc': 'amstradcpc.svg',
+        'msx': 'msx.svg',
+        'msx2': 'msx.svg',
+        'intellivision': 'intellivision.svg',
+        'neo geo pocket color': 'ngpc.svg',
+        'turbografx-16': 'pcengine.svg',
+        'pc engine': 'pcengine.svg',
+        'turbografx-16/pc engine': 'pcengine.svg',
+        'turbografx-16/pc engine cd': 'pcenginecd.svg',
+
+        # Arcade
+        'arcade': 'arcade.svg',
+
+        # Web/Cloud
+        'web browser': 'webbrowser.svg',
+        'google stadia': 'stadia.svg',
+        'onlive game system': 'onlive.svg',
+
+        # VR
+        'oculus vr': 'oculus.svg',
+        'oculus quest': 'oculus.svg',
+        'oculus quest 2': 'oculus.svg',
+        'meta quest 2': 'oculus.svg',
+        'meta quest 3': 'oculus.svg',
+        'oculus go': 'oculus.svg',
+        'gear vr': 'gearvr.svg',
+        'daydream': 'daydream.svg',
+        'steamvr': 'steamvr.svg',
+
+        # Other
+        '3do interactive multiplayer': '3do.svg',
+        'acorn archimedes': 'acorn.svg',
+        'apple ii': 'appleii.svg',
+        'apple iigs': 'appleii.svg',
+        'bbc microcomputer system': 'bbc.svg',
+        'commodore cdtv': 'cdtv.svg',
+        'commodore pet': 'pet.svg',
+        'commodore vic-20': 'vic20.svg',
+        'fm towns': 'fmtowns.svg',
+        'fm-7': 'fm7.svg',
+        'intellivision amico': 'amico.svg',
+        'n-gage': 'ngage.svg',
+        'ouya': 'ouya.svg',
+        'pc-8800 series': 'pc88.svg',
+        'pc-9800 series': 'pc98.svg',
+        'sharp mz-2200': 'sharp.svg',
+        'sharp x1': 'sharp.svg',
+        'sharp x68000': 'x68000.svg',
+        'trs-80': 'trs80.svg',
+        'wonderswan': 'wonderswan.svg',
+        'wonderswan color': 'wonderswancolor.svg',
     }
 
+    # Проверка точных совпадений
+    exact_matches = {
+        '3DO': '3do.svg',
+        '64DD': '64dd.svg',
+        'FDS': 'fds.svg',
+        'PC Engine CD': 'pcenginecd.svg',
+    }
+
+    for key, icon_file in exact_matches.items():
+        if key.lower() == name_lower:
+            return mark_safe(
+                f'<img src="{static_url}{icon_file}" height="24" '
+                f'alt="{platform_name}" title="{platform_name}" '
+                f'style="display: inline-block; vertical-align: middle; border-radius: 4px;">'
+            )
+
+    # Проверка частичных совпадений
     for key, icon_file in platform_map.items():
         if key in name_lower:
             return mark_safe(
